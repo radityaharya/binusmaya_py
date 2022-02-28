73f1ad11a31c7301827a7939e40d83dfe81b6b6d
radityaharya <radityaharya02@gmail.com>
2022-02-28 09:48:29 +0000

    🏛️ Added classes, classComp, classSess functions

--- bimay.py
+++ bimay.py
@@ -1,9 +1,10 @@
+import string
 import requests
 from dateutil import parser
 import time
 import datetime
 import json
-import rich
+
 
 class bimay:
     def __init__(self, roleId, token):
@@ -16,6 +17,7 @@ class bimay:
             token = token[7:]
         self.headers = {
             "Authorization": "Bearer {}".format(token),
+            "institution": "BNS01",
             "Content-Type": "application/json",
             "academicCareer": "RS1",
             "roleId": roleId,
@@ -26,6 +28,7 @@ class bimay:
         }
         self.base_url = "https://apim-bm7-prod.azure-api.net"
         self.schedule_base_url = "https://func-bm7-schedule-prod.azurewebsites.net"
+        self.r = requests.Session()
 
     def config(self, dict):
         """
@@ -35,15 +38,15 @@ class bimay:
         self.conf = dict
 
     def __get_data(self, url, json_data=None, params=None):
-        response = requests.get(
+        response = self.r.get(
             url, params=params, json=json_data, headers=self.headers
         )
-        print(response.text)
         if response.status_code == 200:
             return response.json()
+        raise Exception(response.text)
 
     def __post_data(self, url, json_data=None, params=None):
-        response = requests.post(
+        response = self.r.post(
             url, params=params, json=json_data, headers=self.headers
         )
         if response.status_code == 200:
@@ -52,7 +55,7 @@ class bimay:
 
     def get_latest_academicPeriod(self):
         url = "{}/func-bm7-course-prod/AcademicPeriod/Student".format(self.base_url)
-        response = requests.get(url, headers=self.headers)
+        response = self.r.get(url, headers=self.headers)
         if response.status_code == 200:
             for academicPeriod in response.json():
                 startDate = parser.parse(academicPeriod["termBeginDate"])
@@ -85,12 +88,74 @@ class bimay:
             return fetch_schedule(date_start)
         else:
             schedules = []
-            for date in (date_start + datetime.timedelta(days=x) for x in range(0, (date_end - date_start).days)):
-                for i in range(len(fetch_schedule(date)['Schedule'])):
-                    schedules.append(fetch_schedule(date)['Schedule'][i])
-            first_date_start = min(schedules, key=lambda x: x['dateStart'])['dateStart']
-            data = {
-            "dateStart": first_date_start,
-            "Schedule": schedules
-            }
+            for date in (
+                date_start + datetime.timedelta(days=x)
+                for x in range(0, (date_end - date_start).days)
+            ):
+                for i in range(len(fetch_schedule(date)["Schedule"])):
+                    schedules.append(fetch_schedule(date)["Schedule"][i])
+            first_date_start = min(schedules, key=lambda x: x["dateStart"])["dateStart"]
+            data = {"dateStart": first_date_start, "Schedule": schedules}
             return data
+    
+    # dispose this? feels redundant
+    def get_schedule_month(self, date:datetime.datetime):
+        """
+        --get_schedule_month(date)
+        returns: schedule date from date to date in json format
+        """
+        return self.__post_data(
+            "{}/api/schedule/Month-v1/{}".format(
+                self.schedule_base_url, date.strftime("%Y-%-m-1")
+            ),
+            json_data={},
+        )
+
+    # --ClassComponent-- #
+    def get_class_component_list(self, period:int = None):
+        """
+        -args: period default is current academic period
+        -returns: class component list in json format
+        """
+        if period is None:
+            period = self.get_latest_academicPeriod()["academicPeriod"]
+        else:
+            period = str(period)
+        return self.__get_data(
+            f"{self.base_url}/func-bm7-course-prod/Course/Period/{period}/ClassComponentList/Student"
+        )
+
+    # --classes-- #
+    def get_class_from_component(self, period:int = None, classComponentId:string = None):
+        """
+        -args: period ("LEC", "LAB", "TUT", etc.) default is current academic period
+        -returns: Array of class information dicts [{"classId","classCode","courseName", "courseCode", "lecturers[]", "progressClass", "ssrComponent", "classGroupId"}]
+        """
+        if period is None:
+            period = self.get_latest_academicPeriod()["academicPeriod"]
+        else:
+            period = str(period)
+        return self.__get_data(
+            f"{self.base_url}/func-bm7-course-prod/Course/Period/{period}/Component/{classComponentId}/Student"
+        )
+    
+    def get_class_active(self):
+        '''
+        -returns: Array of active classes
+        '''
+        return self.__get_data(
+            f"{self.base_url}/func-bm7-course-prod/Class/Active/Student"
+        )
+
+
+    # --classSessions-- #
+    def get_class_sessions_from_classId(self, classId:string):
+        """
+        -args: classId
+        -returns: class sessions information from given classId
+        """
+        return self.__get_data(
+            f"{self.base_url}/func-bm7-course-prod/ClassSession/Class/{classId}/Student"
+        )
+    
+
